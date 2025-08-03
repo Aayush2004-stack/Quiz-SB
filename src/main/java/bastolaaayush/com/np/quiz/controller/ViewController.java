05bec5bcf31fa393d8ea74eac7e8f722f9554bba
Aayush <aayushbastola772@gmail.com>
2025-08-03 18:19:11 +0545

    home view and sign up view

--- src/main/java/bastolaaayush/com/np/quiz/controller/ViewController.java
+++ src/main/java/bastolaaayush/com/np/quiz/controller/ViewController.java
@@ -1,4 +1,19 @@
 package bastolaaayush.com.np.quiz.controller;
 
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
 public class ViewController {
+    @GetMapping("/homeView")
+    public String showMainPage(){
+        return "home";
+    }
+    @GetMapping("/signUpView")
+    public String showLoginPage(){
+        return "signUp";
+    }
+
+
 }
