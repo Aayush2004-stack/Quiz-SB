d0161fad9f61e5cbd2a61759b784832b48fb8b77
Aayush <aayushbastola772@gmail.com>
2025-08-17 21:56:54 +0545

    pass questions to add question view

--- src/main/java/bastolaaayush/com/np/quiz/controller/ViewController.java
+++ src/main/java/bastolaaayush/com/np/quiz/controller/ViewController.java
@@ -1,5 +1,9 @@
 package bastolaaayush.com.np.quiz.controller;
 
+import bastolaaayush.com.np.quiz.model.Quiz;
+import bastolaaayush.com.np.quiz.service.QuestionService;
+import bastolaaayush.com.np.quiz.service.QuizService;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -8,22 +12,36 @@ import org.springframework.web.bind.annotation.PathVariable;
 
 @Controller
 public class ViewController {
+
+    @Autowired
+    private QuizService quizService;
+
+    @Autowired
+    private QuestionService questionService;
+
     @GetMapping("/homeView")
     public String showMainPage(){
         return "home";
     }
+
     @GetMapping("/signUpView")
     public String showSignUpPage(){
         return "signUp";
     }
+
     @GetMapping("/logInView")
     public String showLoginPage(){
         return "logIn";
     }
+
     @GetMapping("/addQuestionView/{quizId}")
     public String showAddQuestionPage(@PathVariable int quizId, Model model){
+        Quiz quiz = quizService.getQuizById(quizId)
+                .orElseThrow(() -> new RuntimeException("Quiz not found with id: " + quizId));
 
         model.addAttribute("quizId", quizId);
+        model.addAttribute("questions", questionService.getQuestionsByQuizId(quiz));
+
         return "addQuestion";
     }
 
